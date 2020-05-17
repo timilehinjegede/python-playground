7cdf41c5216c921092f17a37a4c53d1bc3b7e16a
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 07:11:05 +0100

    finished the while loop session

---
+++ basics/while-loop.py
@@ -0,0 +1,10 @@
+# while loop session
+
+# declare a variable(or iterator)
+i = 1
+# condition of the while statement
+while i <= 5:
+    # print the value of the iterator
+    print(i)
+    # increment the iterator
+    i += 1
