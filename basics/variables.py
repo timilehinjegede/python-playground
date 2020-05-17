5c74a96db9293b25ff96c6e4b97659f3d29ab616
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 04:28:03 +0100

    finished the variables session

---
+++ basics/variables.py
@@ -0,0 +1,19 @@
+# first variable
+var_one = 23.3
+# second variable
+var_two = 45
+# third variable
+var_three = 34.24
+# forth variable
+var_four = 'John'
+# fifth variable
+var_five = 'Doe'
+
+# print the sum of forth and fifth variable
+print('His name is: ' + var_four, var_five)
+
+# adding first, second and third variable into var_sum variable
+var_sum = var_one + var_two + var_three
+
+# printing the value of var_sum variable
+print(var_sum)
