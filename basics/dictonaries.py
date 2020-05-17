9aed344ceda57994318fcbbd4495658ad145f94f
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 12:14:54 +0100

    Exploring more dictionary methods

--- basics/dictonaries.py
+++ basics/dictonaries.py
@@ -2,8 +2,6 @@
 
 # creating a dictionary
 phone_book = {'tom': 12345, 'sam': 67890, 'ram': 16273}
-# adding new entries to a dictionary
-phone_book['tam'] = 93930
 # deleting entries in a dictionary
 del phone_book['tom']
 # iterating through the values of a dictionary
@@ -13,4 +11,16 @@ for key in phone_book:
 for key, value in phone_book.items():
     print('Key:', key, 'Value: ', value)
 # deleting all entries in a dictionary
+# adding new entries to a dictionary
+phone_book['tam'] = 93930
+# phone_book.clear()
 phone_book.clear()
+
+
+# exploring more methods in the dictionaries
+# printing the keys of the dictionary
+print(phone_book.keys())
+# printing the values of the dictionary
+print(phone_book.values())
+# gets the value of the specified key in the dictionary
+print(phone_book.get('ram'))
