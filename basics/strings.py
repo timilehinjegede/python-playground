5c6aa0da5a235beb209fde6517f69676cf671b66
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 05:31:01 +0100

    Finished the string methods session

--- basics/strings.py
+++ basics/strings.py
@@ -16,3 +16,24 @@ print(my_name[0:4])  # same as print(my_name[:4])
 # conversion of number to a string
 num = 35
 print(str(num))
+
+# Working with strings methods
+name = 'programming'
+# capitalize the first character of the string
+print(name.capitalize())
+# get the index of the specified character
+print(name.index('g'))
+# convert the specified string to uppercase
+print(name.upper())
+# convert the specified string to lowercase
+print(name.lower())
+# checks if the specified variable is a string
+print(name.isdigit())
+# check if the specified string ends with the specified character
+print(name.endswith('g'))
+# replaced the specified character/characters with the existing character/characters
+print(name.replace('programm', 'cod'))
+# trims the string
+print('      programming    '.strip())  # also .rstrip() and .lstrip()
+# split the specified string using the separator
+print(my_name.split('i'))
