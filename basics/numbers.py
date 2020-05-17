feb5df7bb4f65604045a61bce1f816cf3cd4acec
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 04:32:09 +0100

    Finished the numbers session

---
+++ basics/numbers.py
@@ -0,0 +1,14 @@
+# numbers session
+
+# addition
+print(45 + 5)
+# subtraction
+print(45 - 4)
+# division
+print(17 / 3)
+# multiplication
+print(34 * 2)
+# power
+print(2 ** 6)
+# modulo
+print(16 % 3)
