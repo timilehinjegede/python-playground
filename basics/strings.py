9369fe8926596feb82dc589e8a86bdf998ca6c4d
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 04:56:20 +0100

    Finished the string session

---
+++ basics/strings.py
@@ -0,0 +1,18 @@
+# strings session
+
+# a variable that holds a string
+my_name = 'Timilehin'
+# printing the value of variable my_name
+print(my_name)
+# printing a character at the specified index
+print(my_name[4])
+# printing the length of the string my_name
+print(len(my_name))
+# get substring of my_name
+print(my_name[4:])
+# get substring of my_name
+print(my_name[0:4])  # same as print(my_name[:4])
+
+# conversion of number to a string
+num = 35
+print(str(num))
