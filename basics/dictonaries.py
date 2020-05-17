3f349f5278ada83202d21a13307da0111ce46105
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 12:02:24 +0100

    Removed unused print statements

--- basics/dictonaries.py
+++ basics/dictonaries.py
@@ -2,13 +2,10 @@
 
 # creating a dictionary
 phone_book = {'tom': 12345, 'sam': 67890, 'ram': 16273}
-print(phone_book)
 # adding new entries to a dictionary
 phone_book['tam'] = 93930
-print(phone_book)
 # deleting entries in a dictionary
 del phone_book['tom']
-print(phone_book)
 # iterating through the values of a dictionary
 for key in phone_book:
     print('Key:', key, 'Value:', phone_book[key])
