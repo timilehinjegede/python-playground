0072651d453005bdfb636515208a79d73dbb281d
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-17 05:50:10 +0100

    finished the list session

---
+++ basics/lists.py
@@ -0,0 +1,28 @@
+# list session
+
+# creating a list of fruits
+fruits = ['banana', 'peach', 'orange', 'apple', 'grape']
+# accessing a specific item in the list
+print(fruits[3])
+# mutating a list
+fruits[0] = 'strawberry'  # changes the element at specified index to the assigned element
+print(fruits)
+# adding values to the end of a list
+fruits.append('pineapples')
+print(fruits)
+# adding values to a specified position in the list
+fruits.insert(2, 'pear')
+print(fruits)
+# getting the index of a specified element in the list
+print(fruits.index('pineapples'))
+# accessing the last element of a list
+print(fruits[-1])
+# getting values of a list using the range method
+print(fruits[2:6])
+# joining two lists
+drinks = ['pepsi', 'fanta', 'coke']
+print(fruits+drinks)
+# finding the length of the list
+print(len(fruits))
+# checking if an item is present in the list
+print('pear' in fruits)  # returns a boolean
