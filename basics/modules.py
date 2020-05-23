eb0e27c52a78d63ef357491ad69c0b807c951f78
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-23 10:15:44 +0100

    finished the module session

--- basics/modules.py
+++ basics/modules.py
@@ -2,3 +2,26 @@
 
 # math modules
 import math
+
+# import calendar module
+import calendar
+
+# square root
+print(math.sqrt(16))
+# power of number
+print(math.pow(3, 4))
+# value of pi
+print(math.pi)
+# log of 100 to base 10
+print(math.log10(100))
+# log of 1000 to base 10
+print(math.log10(1000))
+# floor of a number
+print(math.floor(2.3))
+# ceil of a number
+print(math.ceil(2.1))
+
+
+print(calendar.month(2016, 1))
+# checking if a year is a leap year
+print(calendar.isleap(2016))
